0b23207070503068f18a6a72371d445de5e1c675
anton0380 <aa@unelma.ru>
2023-05-26 00:35:48 +0300

    scripts: check video update

--- scripts/check_video.py
+++ scripts/check_video.py
@@ -1,14 +1,21 @@
 from pymediainfo import MediaInfo
-media_info = MediaInfo.parse('test.mp4')
-for track in media_info.tracks:
-  if track.track_type == 'General':
-    data_size = int(track.datasize)
-  if track.track_type == 'Video':
-    video_size = int(track.stream_size)
-  if track.track_type == 'Audio':
-    audio_size = int(track.stream_size)
-print(f"data size = {data_size}")
-print(f"video size = {video_size}")
-print(f"audio size = {audio_size}")
-surplus = data_size - video_size - audio_size
-print(f"surplus = {surplus}")
+
+def check_file(file_name: str) -> bool:
+    media_info = MediaInfo.parse(file_name)
+    for track in media_info.tracks:
+        if track.track_type == 'General':
+            data_size = int(track.datasize)
+        if track.track_type == 'Video':
+            video_size = int(track.stream_size)
+        if track.track_type == 'Audio':
+            audio_size = int(track.stream_size)
+    surplus = data_size - video_size - audio_size
+    return surplus
+
+from pathlib import Path
+ 
+directory = ''
+pathlist = Path(directory).glob('*.mp4')
+for path in pathlist:
+    res = check_file(path)
+    print(str(path) + ' ' + f"{'ok' if res >= 0 else 'error'} ({res})")
