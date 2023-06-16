f81777f2d5bde5c06117d5cd227b62762f178740
anton0380 <aa@unelma.ru>
2023-06-16 23:41:01 +0300

    mysite: pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
@@ -15,10 +18,6 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404('No Post found.')
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
