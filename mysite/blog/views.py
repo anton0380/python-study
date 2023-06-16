49b8f033737c43d3f59ba8f7335b4ccbb31c26b5
anton0380 <aa@unelma.ru>
2023-06-16 23:49:53 +0300

    mysite: pagination exceptions

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not a integer show first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # show last page if page is not in range
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
