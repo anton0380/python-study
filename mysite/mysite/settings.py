5cb1c235b95b573cf598ebd5adc64df1aa0ca6b1
anton0380 <aa@unelma.ru>
2026-01-12 08:12:45 +0300

    mysite: in comment add sql logging in console

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,3 +116,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     },
+# }
